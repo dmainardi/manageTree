5fbdf7bd4c5b96c65f6c859841fcc5451d78ae90
Davide Mainardi <ingmainardi@live.com>
2015-12-20 16:00:35 +0100

    Created the presenters (backing beans) and the web pages for internal element.

---
+++ src/main/java/com/dmainardi/manageTree/presentation/internalElement/InternalElementPresenter.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2015 Davide Mainardi <ingmainardi at live.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.dmainardi.manageTree.presentation.internalElement;
+
+import com.dmainardi.manageTree.business.boundary.InternalElementService;
+import com.dmainardi.manageTree.business.entity.InternalElement;
+import java.io.Serializable;
+import javax.faces.flow.FlowScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Davide Mainardi <ingmainardi at live.com>
+ */
+@Named
+@FlowScoped("internalElement")
+public class InternalElementPresenter implements Serializable {
+    @Inject
+    InternalElementService internalElementService;
+    
+    private InternalElement internalElement;
+        
+    public void deleteInternalElement(InternalElement internalElement) {
+        internalElementService.deleteInternalElement(internalElement);
+    }
+    
+    public String saveInternalElement() {
+        internalElementService.saveInternalElement(internalElement);
+        
+        return "internalElements";
+    }
+    
+    public String detailInternalElement(Long id) {
+        if (id == null)
+            internalElement = new InternalElement();
+        else
+            internalElement = internalElementService.readInternalElement(id);
+        
+        return "internalElement";
+    }
+
+    public InternalElement getInternalElement() {
+        return internalElement;
+    }
+
+    public void setInternalElement(InternalElement internalElement) {
+        this.internalElement = internalElement;
+    }
+}
