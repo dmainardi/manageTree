6011b0d1c626eece5d01b9043d93b22c4b361898
Davide Mainardi <ingmainardi@live.com>
2015-12-20 15:46:36 +0100

    Created the presenters (backing beans) and the web pages for unit of measure.

---
+++ src/main/java/com/dmainardi/manageTree/presentation/unitMeasure/UnitMeasurePresenter.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2015 Davide Mainardi <ingmainardi at live.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.dmainardi.manageTree.presentation.unitMeasure;
+
+import com.dmainardi.manageTree.business.boundary.UnitMeasureService;
+import com.dmainardi.manageTree.business.entity.UnitMeasure;
+import java.io.Serializable;
+import javax.faces.flow.FlowScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Davide Mainardi <ingmainardi at live.com>
+ */
+@Named
+@FlowScoped("unitMeasure")
+public class UnitMeasurePresenter implements Serializable {
+    @Inject
+    UnitMeasureService unitMeasureService;
+    
+    private UnitMeasure unitMeasure;
+        
+    public void deleteUnitMeasure(UnitMeasure unitMeasure) {
+        unitMeasureService.deleteUnitMeasure(unitMeasure);
+    }
+    
+    public String saveUnitMeasure() {
+        unitMeasureService.saveUnitMeasure(unitMeasure);
+        
+        return "unitsMeasure";
+    }
+    
+    public String detailUnitMeasure(Long id) {
+        if (id == null)
+            unitMeasure = new UnitMeasure();
+        else
+            unitMeasure = unitMeasureService.readUnitMeasure(id);
+        
+        return "unitMeasure";
+    }
+
+    public UnitMeasure getUnitMeasure() {
+        return unitMeasure;
+    }
+
+    public void setUnitMeasure(UnitMeasure unitMeasure) {
+        this.unitMeasure = unitMeasure;
+    }
+}
